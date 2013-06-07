1a67a8e716901849bff025a42f653b756d44bb87
Stephen Johnson <steve@steveasleep.com>
2013-06-06 22:18:17 -0700

    Move grep soln to solns

--- exercises/grep/grep_job.py
+++ exercises/grep/grep_job.py
@@ -20,33 +20,13 @@ We are using a flag (-e) instead of a positional argument because mrjob does
 not provide a straightforward way to do that.
 """
 
-import re
-from optparse import OptionError
-
 from mrjob.job import MRJob
-from mrjob.protocol import RawValueProtocol
 
 
 class MRGrepJob(MRJob):
 
-    INPUT_PROTOCOL = RawValueProtocol  # this is the default
-    OUTPUT_PROTOCOL = RawValueProtocol
-
-    def configure_options(self):
-        super(MRGrepJob, self).configure_options()
-        self.add_passthrough_option('-e', '--expression')
-
-    def load_options(self, *args, **kwargs):
-        super(MRGrepJob, self).load_options(*args, **kwargs)
-        if not self.options.expression:
-            raise OptionError("The -e flag is required.")
-
-    def mapper_init(self):
-        self.re = re.compile(self.options.expression)
-
     def mapper(self, _, line):
-        if self.re and self.re.match(line):
-            yield None, line
+        yield None, line
 
 
 if __name__ == '__main__':
