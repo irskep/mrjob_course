db22d1f1ff481a942947dca4061aba3c20e7303c
Stephen Johnson <steve@steveasleep.com>
2013-06-11 22:54:55 -0700

    New wc_super_optimized exercise

---
+++ exercises/wc_super_optimized/wc_job.py
@@ -0,0 +1,34 @@
+"""
+Use ``mapper_init()`` and ``mapper_final()`` to write a more efficient version
+of the wc exercise.
+"""
+
+from mrjob.job import MRJob
+
+
+class MRWordCountJob(MRJob):
+
+    def mapper_init(self):
+        self.chars = 0
+        self.words = 0
+        self.lines = 0
+
+    def mapper(self, _, line):
+        self.chars += len(line)
+        self.words += sum(1 for word in line.split() if word.strip())
+        self.lines += 1
+
+    def mapper_final(self):
+        yield 'characters', self.chars
+        yield 'words', self.words
+        yield 'lines', self.lines
+
+    def combiner(self, key, values):
+        yield key, sum(values)
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    MRWordCountJob.run()
